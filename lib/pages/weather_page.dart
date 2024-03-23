66a2d2807e884d6c48b2f23ba478be67304a7246
petrushkins <petrushenkokris@yandex.ru>
2024-03-24 02:08:50 +0300

    feat: Добавлена тестовая версия приложения

---
+++ lib/pages/weather_page.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_weather_app/models/weather_model.dart';
+import 'package:flutter_weather_app/services/weather_service.dart';
+
+class WeatherPage extends StatefulWidget {
+  const WeatherPage({super.key});
+
+  @override
+  State<WeatherPage> createState() => _WeatherPageState();
+}
+
+class _WeatherPageState extends State<WeatherPage> {
+  final _weatherService = WeatherService("7a1947745118a5fca6d8e156f4724528");
+  Weather? _weather;
+
+  _fetchWeather() async {
+    String cityName = await _weatherService.getCurrentCity();
+
+    try {
+      final weather = await _weatherService.getWeather(cityName);
+      setState(() {
+        _weather = weather;
+      });
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+
+    _fetchWeather();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Center(
+            child: Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Text(_weather?.cityName ?? "loading city..."),
+        Text("${_weather?.temperature.round()}°C")
+      ],
+    )));
+  }
+}
